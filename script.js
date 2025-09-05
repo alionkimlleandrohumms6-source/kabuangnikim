aadec032e66caa68c1f6ce6168038be639b9cf6c
alionkimlleandrohumms6-source <alionkimlleandro.humms6@gmail.com>
2025-09-05 14:38:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes GAHAHAHAHAbuang.\n\nAnyway, Happy Birthday! 18 naka! playsafe na kay pwede na makulong jok btw I'm hoping you're doing well especially sa acads nimo!\n\nMay God take care of you everyday,everysecond,everywhere.`;
+
+
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
